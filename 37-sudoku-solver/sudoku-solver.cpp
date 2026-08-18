8d949d7ab5cfbde7190dfdcabc0c5656e427caf9
MANJUPRANAV-7 <manjupranav7814@gmail.com>
2026-08-18 23:12:10 +0530

    Time: 260 ms (68.70%) | Memory: 9 MB (15.20%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool isSafe(vector<vector<char>>& board, int row, int col, char ch) {
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == ch)
+                return false;
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == ch)
+                return false;
+        }
+
+        // Check 3x3 box
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == ch)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+
+                    for (char ch = '1'; ch <= '9'; ch++) {
+
+                        if (isSafe(board, i, j, ch)) {
+                            board[i][j] = ch;
+
+                            if (solve(board))
+                                return true;
+
+                            // Backtrack
+                            board[i][j] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};

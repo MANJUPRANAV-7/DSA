796d33ddcf01d8ee50d2a6cd77d7ae3344c9fbf9
MANJUPRANAV-7 <manjupranav7814@gmail.com>
2026-08-21 00:37:41 +0530

    Time: 759 ms (5.19%) | Memory: 463.8 MB (5.21%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
+set <vector<int>> s;
+    void combSum(vector<vector<int>> &ans ,vector<int>& comb , int t , vector<int> candidates , int i){
+        if(i==candidates.size()  || t<0){
+            return ;
+        }
+        if(t==0){
+            if(s.find(comb)==s.end()){
+            ans.push_back({comb});
+            s.insert(comb);
+            return;
+            }
+        }
+        comb.push_back({candidates[i]});
+        combSum(ans , comb , t-candidates[i] , candidates , i+1);
+        combSum(ans , comb , t-candidates[i] , candidates , i);
+        comb.pop_back();
+        combSum(ans , comb , t , candidates , i+1);
+    }
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-
-        function<void(int, int)> backtrack = [&](int start, int target) {
-            if (target == 0) {
-                ans.push_back(curr);
-                return;
-            }
-
-            for (int i = start; i < candidates.size(); i++) {
-                if (candidates[i] > target)
-                    continue;
-
-                curr.push_back(candidates[i]);
-
-                // i, not i+1, because we can reuse the same number
-                backtrack(i, target - candidates[i]);
-
-                curr.pop_back();
-            }
-        };
-
-        backtrack(0, target);
+        vector<vector<int>> ans ;
+        vector <int> comb;
+        combSum(ans , comb , target , candidates , 0);
         return ans;
     }
 };
